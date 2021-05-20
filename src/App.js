8138286efa68b5346337ce09d64c0b44f9402388
hugo <hugopage0017@gmail.com>
2021-05-20 17:23:05 +1200

    testing logout fix

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
         const accessToken = res.tokens.accessToken.accessToken;
         localStorage.setItem('token', accessToken);
         setLogin(true);
-      }).catch(() =>  { return err; });
+      }).catch((err) =>  { return err; });
     }
   },[]);
 
