840874d1cfae3fd0e6c359896f3a9f8eb5ce859d
methugop <hugo.page@metservice.com>
2021-11-13 13:54:51 +1300

    set image link to cdn endpoint

--- src/metadata/Endpoints.js
+++ src/metadata/Endpoints.js
@@ -1,7 +1,7 @@
 export const apiEndpoint = 'https://io8flgqkmk.execute-api.ap-southeast-2.amazonaws.com/test';
-export const imageBucket = 'https://geonet-volcano-images.s3-ap-southeast-2.amazonaws.com';
+export const imageBucket = 'https://d3tdg999z8uo5q.cloudfront.net';
 export const loopyBucket = 'https://loopy-files.s3-ap-southeast-2.amazonaws.com';
 export const redirectUri = window.location.origin;
 
 export const gnsEndpont = 'https://data.geonet.org.nz/camera/volcano/images';
-export const gnsRestEndpoint = 'https://api.geonet.org.nz'
+export const gnsRestEndpoint = 'https://api.geonet.org.nz';
