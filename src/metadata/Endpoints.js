bebd1b977428e8de9be5c94b4daf4bfe05c7c737
methugop <hugo.page@metservice.com>
2021-11-19 16:25:12 +1300

    prod endpoint set

--- src/metadata/Endpoints.js
+++ src/metadata/Endpoints.js
@@ -1,4 +1,4 @@
-export const apiEndpoint = 'https://2p4rq8q5g8.execute-api.ap-southeast-2.amazonaws.com/prod/v0/rest';
+export const apiEndpoint = 'https://nsg4nwonug.execute-api.ap-southeast-2.amazonaws.com/prod/v0/rest';
 export const imageBucket = 'https://d1bv4auv9bbqce.cloudfront.net';
 export const loopyBucket = 'https://loopy-files.s3-ap-southeast-2.amazonaws.com';
 export const redirectUri = window.location.origin;
