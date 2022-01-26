ddfab387c3d7b7a4ef05b8ccfd9bc4ef5f47362d
methugop <hugo.page@metservice.com>
2022-01-27 12:09:32 +1300

    new rest link

--- src/metadata/Endpoints.js
+++ src/metadata/Endpoints.js
@@ -1,4 +1,4 @@
-export const apiEndpoint = 'https://nsg4nwonug.execute-api.ap-southeast-2.amazonaws.com/prod/v0/rest';
+export const apiEndpoint = 'https://g43sxzej92.execute-api.ap-southeast-2.amazonaws.com/prod/v0/rest';
 export const imageBucket = 'https://d2e7bw4oknorz6.cloudfront.net';
 export const loopyBucket = 'https://loopy-files.s3-ap-southeast-2.amazonaws.com';
 export const redirectUri = window.location.origin;
