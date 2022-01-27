080818fd1f9bd6bfffe261325616ca8a76203a82
methugop <hugo.page@metservice.com>
2022-01-27 16:51:41 +1300

    redeploying test

--- src/metadata/Endpoints.js
+++ src/metadata/Endpoints.js
@@ -1,4 +1,4 @@
-export const apiEndpoint = 'https://dsg0s7ekl2.execute-api.ap-southeast-2.amazonaws.com/prod/v0/rest';
+export const apiEndpoint = 'https://koztdf20e6.execute-api.ap-southeast-2.amazonaws.com/prod/v0/rest';
 export const imageBucket = 'https://d3tdg999z8uo5q.cloudfront.net';
 export const loopyBucket = 'https://loopy-files.s3-ap-southeast-2.amazonaws.com';
 export const redirectUri = window.location.origin;
@@ -6,4 +6,4 @@ export const redirectUri = window.location.origin;
 export const gnsEndpont = 'https://data.geonet.org.nz/camera/volcano/images';
 export const FITS_ENDPOINT = 'https://fits.geonet.org.nz';
 export const gnsRestEndpoint = 'https://api.geonet.org.nz';
-export const userSavedImagesCDN = 'https://d3rn1cnf3wdnbk.cloudfront.net'
+export const userSavedImagesCDN = 'https://d1xsjyv3hh3owx.cloudfront.net'
