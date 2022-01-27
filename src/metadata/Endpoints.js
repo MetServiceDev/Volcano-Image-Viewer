a2227c4075bfb0df9b675b5bb57d24e8677b54bf
methugop <hugo.page@metservice.com>
2022-01-27 13:27:50 +1300

    new user image cdn

--- src/metadata/Endpoints.js
+++ src/metadata/Endpoints.js
@@ -6,4 +6,4 @@ export const redirectUri = window.location.origin;
 export const gnsEndpont = 'https://data.geonet.org.nz/camera/volcano/images';
 export const FITS_ENDPOINT = 'https://fits.geonet.org.nz';
 export const gnsRestEndpoint = 'https://api.geonet.org.nz';
-export const userSavedImagesCDN = 'https://d3rn1cnf3wdnbk.cloudfront.net'
+export const userSavedImagesCDN = 'https://d2luipjouy3ns5.cloudfront.net'
