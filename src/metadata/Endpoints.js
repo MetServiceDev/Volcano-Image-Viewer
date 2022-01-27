8e4e4de9fd70aefac6f633624c5f33bd49643bf2
methugop <hugo.page@metservice.com>
2022-01-27 13:15:23 +1300

    ready for sandpit amplify deploy

--- src/metadata/Endpoints.js
+++ src/metadata/Endpoints.js
@@ -1,4 +1,4 @@
-export const apiEndpoint = 'https://zrdqlzn6f5.execute-api.ap-southeast-2.amazonaws.com/prod/v0/rest';
+export const apiEndpoint = 'https://dsg0s7ekl2.execute-api.ap-southeast-2.amazonaws.com/prod/v0/rest';
 export const imageBucket = 'https://d3tdg999z8uo5q.cloudfront.net';
 export const loopyBucket = 'https://loopy-files.s3-ap-southeast-2.amazonaws.com';
 export const redirectUri = window.location.origin;
