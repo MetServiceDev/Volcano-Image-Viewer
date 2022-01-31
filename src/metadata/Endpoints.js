103fc64f0cf84c59fb1186d8cb22893077aa952b
methugop <hugo.page@metservice.com>
2022-02-01 08:40:08 +1300

    cutting over to prod

--- src/metadata/Endpoints.js
+++ src/metadata/Endpoints.js
@@ -1,9 +1,9 @@
-export const apiEndpoint = 'https://koztdf20e6.execute-api.ap-southeast-2.amazonaws.com/prod/v0/rest';
-export const imageBucket = 'https://d3tdg999z8uo5q.cloudfront.net';
+export const apiEndpoint = 'https://g43sxzej92.execute-api.ap-southeast-2.amazonaws.com/prod/v0/rest';
+export const imageBucket = 'https://d2e7bw4oknorz6.cloudfront.net';
 export const loopyBucket = 'https://loopy-files.s3-ap-southeast-2.amazonaws.com';
 export const redirectUri = window.location.origin;
 
 export const gnsEndpont = 'https://data.geonet.org.nz/camera/volcano/images';
 export const FITS_ENDPOINT = 'https://fits.geonet.org.nz';
 export const gnsRestEndpoint = 'https://api.geonet.org.nz';
-export const userSavedImagesCDN = 'https://d1xsjyv3hh3owx.cloudfront.net';
+export const userSavedImagesCDN = 'https://d2ig5i70fribjk.cloudfront.net';
