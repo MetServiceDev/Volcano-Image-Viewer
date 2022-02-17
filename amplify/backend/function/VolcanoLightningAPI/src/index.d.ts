f754591c2a9bb4b35a4e431446aef24859763f6b
methugop <hugo.page@metservice.com>
2022-02-17 13:34:33 +1300

    installing typescript on preBuild

--- amplify/backend/function/VolcanoLightningAPI/src/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const handler: () => Promise<{
-    statusCode: number;
-    headers: {
-        'Content-Type': string;
-        'Access-Control-Allow-Origin': string;
-    };
-    body: string;
-}>;
-export { handler };
