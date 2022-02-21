1d886c4f1245a4994c4399a803fa97a7c20fb569
methugop <hugo.page@metservice.com>
2022-02-21 15:49:17 +1300

    merging to v2.0.2

---
+++ amplify/#current-cloud-backend/function/VolcanoLightningAPI/src/lightningFormatter.d.ts
@@ -0,0 +1,27 @@
+interface Feature {
+    geometry: {
+        coordinates: [number, number];
+        type: string;
+    };
+    properties: {
+        name: string;
+        region: string;
+        type: string;
+        twentyKStrikes: number;
+        hundredKStrikes: number;
+        timestamp: Date;
+        alertLevel: string;
+    };
+    type: string;
+}
+interface GeoJSON {
+    type: string;
+    features: Feature[];
+}
+export declare const extractFromDom: (domArray: HTMLCollectionOf<Element>, alertLevel: string) => Feature[];
+/**
+ * Promise based function to return recent lightning strikes around VAAC Regions
+ * @returns {GeoJSON}
+ */
+export declare const fetchLightningStrikes: () => Promise<GeoJSON>;
+export {};
