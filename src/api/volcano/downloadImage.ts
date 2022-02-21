fa9949fba1e443d3090ba003062fee1fccf45750
methugop <hugo.page@metservice.com>
2022-02-21 15:28:00 +1300

    added upload date validator on download image

--- src/api/volcano/downloadImage.ts
+++ src/api/volcano/downloadImage.ts
@@ -1,13 +1,23 @@
+import moment from 'moment';
 import { Thumbnail } from './headers';
 
-const downloadImage = async(link: string): Promise<Thumbnail> => {
+const downloadImage = async(link: string, idx: number): Promise<Thumbnail> => {
     const call = await fetch(link);
     const size = Number(call.headers.get('Content-Length'));
     const timestamp = call.headers.get('x-amz-meta-timestamp');
-    const hasntUpdated = call.headers.get('x-amz-meta-hasntupdated');
+    let hasntUpdated = JSON.parse(call.headers.get('x-amz-meta-hasntupdated') as string);
+    const uploadedAt =  call.headers.get('x-amz-meta-uploadedat');
+    if (idx === 11) {
+        if (uploadedAt) {
+            const uploadTime = moment(uploadedAt).toDate();
+            if(moment().diff(uploadTime, 'minutes') >= 16) {
+                hasntUpdated = true;
+            }
+        }
+    }
     const blob = await call.blob();
     const src = URL.createObjectURL(blob);
-    return { src, size, timestamp, hasntUpdated:JSON.parse(hasntUpdated as string)} as Thumbnail;
+    return { src, size, timestamp, uploadedAt, hasntUpdated } as Thumbnail;
 };
 
 export default downloadImage;
